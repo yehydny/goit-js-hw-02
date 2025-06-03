8076e5d5176296865541b2292060cdd8b9049808
Dmytro Osadchuk <144940208+yehydny@users.noreply.github.com>
2025-06-04 00:54:52 +0300

    add test1 and test2

---
+++ js/task-1.js
@@ -0,0 +1,14 @@
+function checkStorage(available, ordered) {
+    if (available < ordered) {
+      return `Not enough goods in stock!`;
+    }
+    else {
+      return `Order is processed, our manager will contact you`;
+    }
+  }
+  
+console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
+console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
+console.log(makeTransaction(10, 5000, 8000)); // "Insufficient funds!"
+console.log(makeTransaction(8, 2000, 10000)); // "Insufficient funds!"
+console.log(makeTransaction(10, 500, 5000)); // "You ordered 10 droids worth 5000 credits!"
